12aff6da3b19184a96a39cc1ab1de426cad0f7c7
gvs0709 <gvillaress@gmail.com>
2018-12-10 18:45:49 -0200

    Algum avanço na estrutura do esqueleto, ainda não é compilável

--- Trabalho_3/memory_management_simulator.c
+++ Trabalho_3/memory_management_simulator.c
@@ -18,7 +18,7 @@ enum{
 };
 
 typedef struct process_pcb{
-    int PID, /*PPID, PRIORITY, STATUS, P_TIME, E_TIME, IOITERATOR, IOTIME[MAX_VIRTUAL_PAGES], AUX, OLD_ITERATOR, */VIRTUAL_PAGES, SIZE;
+    int PID, /*PPID, PRIORITY, STATUS, P_TIME, E_TIME, IOITERATOR, IOTIME[MAX_VIRTUAL_PAGES], AUX, OLD_ITERATOR, */VIRTUAL_PAGES, PAGE_NUMBER[MAX_VIRTUAL_PAGES], SIZE;
     //char IOLIST[MAX_VIRTUAL_PAGES];
     //bool PENDINGIO;
 }PCB;
@@ -41,9 +41,11 @@ typedef struct swap_memory{
 unsigned int pid_counter = 100;
 PCB *bootstrapper, *process_list[MAX_PROCESSES];
 MEMORY *mainMemory;
+SWAP *swapMemory;
 
-PCB *Assemble_PCB(int pid, int ppid, int priority, int status, int p_time, int iterator){
+PCB *Assemble_PCB(int pid){
     //int aux = randombytes_uniform(MAX_IOREQUESTS+1); // Decides how many I/O requests will be made randomly, varies from 0 to MAX_IOREQUESTS - 1
+    int aux;
 
     bootstrapper = malloc(sizeof(PCB));
     bootstrapper->PID = pid;
@@ -57,34 +59,27 @@ PCB *Assemble_PCB(int pid, int ppid, int priority, int status, int p_time, int i
     bootstrapper->VIRTUAL_PAGES = randombytes_uniform(MAX_VIRTUAL_PAGES) + 1; // Decides how many virtual pages the process will have. Varies from 1 to MAX_VIRTUAL_PAGES
     bootstrapper->SIZE = bootstrapper->VIRTUAL_PAGES * PAGE_SIZE;
 
-    // To uncomment this section MAX_IOREQUESTS must be MAX_SERVICE_TIME - 1
-    /*if (bootstrapper->P_TIME < MAX_IOREQUESTS){
-        aux = randombytes_uniform(p_time + 1); // Executes the 'for' bellow a random number of times (ranges from 0 to P_TIME times)
+    for(int j = 0; j < MAX_VIRTUAL_PAGES; j++){
+        bootstrapper->PAGE_NUMBER[j] = 0;
     }
 
-    else{
-        aux = randombytes_uniform(MAX_IOREQUESTS + 1); // Executes the 'for' bellow a random number of times (ranges from 0 to MAX_IOREQUESTS times)
-    }*/
+    for (int i = 0; i < bootstrapper->VIRTUAL_PAGES; i++) {
+        aux = randombytes_uniform(MAX_VIRTUAL_PAGES);
+        //bootstrapper->PAGE_NUMBER[i] = 1; //randombytes_uniform((const uint32_t) bootstrapper->P_TIME - 1) + 1; // Ranges from 1 to P_TIME-1
 
-    /*for (int i = 0; i < aux; ++i) {
-        bootstrapper->IOTIME[i] = randombytes_uniform((const uint32_t) bootstrapper->P_TIME - 1) + 1; // Ranges from 1 to P_TIME-1
-
-        if (i == 0){
-            printf("| pid: %d, IOTIME[%d]: %d, ", pid, i, bootstrapper->IOTIME[i]);
+        if (bootstrapper->PAGE_NUMBER[aux] == 0){
+            bootstrapper->PAGE_NUMBER[aux] = 1; // Creates a page with id aux, that ranges from 0 to 63
         }
 
         else{
-            for (int j = 0; j < i; ++j) {
-                if (bootstrapper->IOTIME[i] == bootstrapper->IOTIME[j]){
-                    bootstrapper->IOTIME[i] = randombytes_uniform((const uint32_t) bootstrapper->P_TIME - 1) + 1;
-                    j = -1; // Checks IOTIME from the start again
-                }
+            while(bootstrapper->PAGE_NUMBER[aux] != 0){
+                aux = randombytes_uniform(MAX_VIRTUAL_PAGES);
             }
 
-            printf("| pid: %d, IOTIME[%d]: %d, ", pid, i, bootstrapper->IOTIME[i]);
+            bootstrapper->PAGE_NUMBER[aux] = 1;
         }
 
-        switch (select_IO[randombytes_uniform(IO_TYPE)]){
+        /*switch (select_IO[randombytes_uniform(IO_TYPE)]){
             case T_DISC:
                 bootstrapper->IOLIST[i] = DISC;
                 break;
@@ -115,8 +110,8 @@ PCB *Assemble_PCB(int pid, int ppid, int priority, int status, int p_time, int i
                 //break;
         }
 
-        printf("IOLIST[%d]: %c |\n", i, bootstrapper->IOLIST[i]);
-    }*/
+        printf("IOLIST[%d]: %c |\n", i, bootstrapper->IOLIST[i]);*/
+    }
 
     /*printf("\n");
 
@@ -154,7 +149,8 @@ PCB *Assemble_PCB(int pid, int ppid, int priority, int status, int p_time, int i
 }
 
 unsigned int create_frame(MEMORY *caller, FRAME **leaf){
-    unsigned int FRAME_ID = caller->NFRAMES + 1, SIZE = caller->FREE_SPACE;
+    unsigned int FRAME_ID = caller->NFRAMES + 1, SIZE = caller->SIZE;
+    //void *SIZE = caller->SIZE;
 
     if(*leaf == NULL){
         *leaf = (FRAME*) malloc(sizeof(FRAME));
@@ -182,8 +178,95 @@ void initialize_memory(){
     mainMemory = malloc(sizeof(MEMORY));
     mainMemory->SIZE = MAX_VIRTUAL_PAGES * PAGE_SIZE; // Main memory size in KB. Default is 64 * 4 = 256 KB
     mainMemory->FRAME_ROOT = NULL;
-    mainMemory->FREE_SPACE = mainMemory->SIZE; // The memory is empty
+    //mainMemory->FREE_SPACE = mainMemory->SIZE; // The memory is empty
     mainMemory->NFRAMES = 0;
 
     mainMemory->NFRAMES = create_frame(mainMemory, &mainMemory->FRAME_ROOT); // Creates the first frame occupying all the memory
+}
+
+void Terminate(){
+    for (int i = 0; i < MAX_PROCESSES; i++){
+        free(process_list[i]);
+    }
+}
+
+int main(int argc, char const *argv[]){
+    //int P[20],burst_time[20],quantum,n; // Round Robin waiting time/ turnaround time variables/ parameters
+    //start_t = clock();
+
+    //printf("==> Simulator start time: %6.3f\n", (start_t * 1000. / CLOCKS_PER_SEC));
+    //printf("\n");
+
+    if (sodium_init() < 0) {
+        printf("Panic! The Sodium library couldn't be initialized, it is not safe to use");
+        return 1;
+    }
+
+    /*for (int i = 0; i < MAX_PROCESSES; ++i) {
+        high_queue[i] = NULL;
+        low_queue[i] = NULL;
+        disc_queue[i] = NULL;
+        tape_queue[i] = NULL;
+        printer_queue[i] = NULL;
+        exited_cpu[i] = NULL;
+    }
+
+    select_IO[0] = T_DISC;
+    select_IO[1] = T_TAPE;
+    select_IO[2] = T_PRINTER;*/
+
+    pthread_t tid_sistema[NTHREADS];
+    int *arg;
+
+    for(int t = 0; t < NTHREADS; t++) {
+        //printf("--Aloca e preenche argumentos para thread %d\n", t);
+        arg = malloc(sizeof(int));
+
+        if (arg == NULL) {
+            printf("--ERROR: malloc()\n");
+            exit(-1);
+        }
+
+        *arg = t;
+
+        //if (t == 0) {
+            if (pthread_create(&tid_sistema[t], NULL, Create_Process, (void *) arg)) {
+                printf("--ERROR: pthread_create()\n");
+                exit(-1);
+            }
+        //}
+
+        /*if (t == 1){
+            if (pthread_create(&tid_sistema[t], NULL, Scheduler, (void *) arg)) {
+                printf("--ERROR: pthread_create()\n");
+                exit(-1);
+            }
+        }*/
+    }
+
+    /*printf("==> Chamei CPU!!!\n");
+    CPU(high_queue[0]);
+    printf("==> Cabou CPU\n");*/
+
+    for (int t = 0; t < NTHREADS; t++) {
+        if (pthread_join(tid_sistema[t], NULL)) {
+            printf("--ERROR: pthread_join() \n");
+            exit(-1);
+        }
+    }
+
+    Terminate();
+    /*end_t = clock();
+
+    printf("\n");
+    printf("==> Simulator end time: %6.3f\n", (end_t * 1000. / CLOCKS_PER_SEC));
+    printf("\n");
+
+    total_t = end_t - start_t;
+
+    printf("\n");
+    printf("==> Simulator total time: %6.3f s\n", (total_t * 1000. / CLOCKS_PER_SEC));
+    printf("\n");*/
+
+    pthread_exit(NULL);
 }
